3620d961c5caacc0d8ddab3eac4833946435055b
nibnualdi <nibnualdi@gmail.com>
2024-05-27 11:57:03 +0700

    first commit: setup express + ts & install all the basic depedencies

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+console.log("[server]: Wait a moment!");
+
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome!!");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
