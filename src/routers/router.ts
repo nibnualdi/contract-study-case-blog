a2f68d1ec585a5a3f9aadeae1df7ca03e44f06e6
nibnualdi <nibnualdi@gmail.com>
2024-05-30 09:19:02 +0700

    feat: add profile route

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -5,6 +5,7 @@ import uploadRouter from "./upload";
 import categoryRouter from "./category";
 import userRouter from "./user";
 import authRouter from "./auth";
+import { getById } from "../controllers/userController";
 
 const router = express.Router();
 
@@ -12,5 +13,9 @@ router.use("/upload", uploadRouter);
 router.use("/category", categoryRouter);
 router.use("/user", userRouter);
 router.use("/auth", authRouter);
+
+// custom endpoint
+// example http://localhost:3000/v1/startfromhere
+router.get("/profile/:id", getById)
 
 export default router;
