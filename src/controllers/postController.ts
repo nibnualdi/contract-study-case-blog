e4c6f4ee282bef9a8a3a378341fd4f2bd8c758b1
nibnualdi <nibnualdi@gmail.com>
2024-05-30 14:41:10 +0700

    feat: add get all post route

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -3,6 +3,7 @@ import Posts from "../db/models/posts";
 import PostCategories, { PostCategoriesCreationAttributes } from "../db/models/postcategories";
 import { NullishPropertiesOf } from "sequelize/types/utils";
 import { AbstractDataTypeConstructor } from "sequelize";
+import Categories from "../db/models/categories";
 
 const create = async (req: Request, res: Response) => {
   const title = req.body.title;
@@ -41,4 +42,35 @@ const create = async (req: Request, res: Response) => {
   }
 };
 
-export { create };
+const getAll = async (req: Request, res: Response) => {
+  const page: number = Number(req.query.page) || 1;
+  const pageSize: number = Number(req.query.pageSize) || 2;
+  const title: string = (req.query.title as string) || "";
+  const category: string = (req.query.category as string) || "";
+  const offset = (page - 1) * pageSize;
+
+  try {
+    const { rows, count } = await Posts.findAndCountAll({
+      include: [
+        {
+          model: Categories,
+          through: { where: { categoryId: category } },
+        },
+      ],
+      offset,
+      limit: pageSize,
+      where: title ? { title } : {},
+    });
+
+    res.status(200).json({
+      code: 200,
+      message: `${count} data sudah diterima`,
+      count: count,
+      data: rows,
+    });
+  } catch (error) {
+    res.json({ error });
+  }
+};
+
+export { create, getAll };
--- src/routers/post.ts
+++ src/routers/post.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { create } from "../controllers/postController";
+import { create, getAll } from "../controllers/postController";
 
 const router = express.Router();
 
 router.post("/", create);
+router.get("/", getAll);
 
 export default router;
